0d12966efe9537361ea7cf4ba779018120c9318b
Julian <juliangabriel@gmail.com>
2026-01-22 17:51:41 +0000

    xsd

---
+++ Clases/Clases/HTML,CSS, script/login.php
@@ -0,0 +1,14 @@
+
+<html>
+	<head>
+		<title>DesdeApp</title>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h1>Respuesta desde PHP</h1>
+		<p>Tu nombre de usuario es <b></b></p>
+		<p>Tu contraseña es <b></b></p>
+		<p>Tu selección ha sido <b></b></p>
+	</body>
+</html>                                                                                                    
+~                     
